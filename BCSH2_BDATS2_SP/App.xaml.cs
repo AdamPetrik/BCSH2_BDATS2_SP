7c4945fd5f30259886fa8983304f300ddaa90af7
AdamPetrik <146947408+AdamPetrik@users.noreply.github.com>
2025-10-20 19:25:24 +0200

    Project created + gitignore

---
+++ BCSH2_BDATS2_SP/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace BCSH2_BDATS2_SP
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
