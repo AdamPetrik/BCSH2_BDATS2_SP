bb850046e0eb3c77b7777bd849f0a09f8b4dddd2
AdamPetrik <146947408+AdamPetrik@users.noreply.github.com>
2025-11-03 19:42:41 +0100

    Projekt zalozen

---
+++ SpravaObjednavek_GUI_App/LoginForm.cs
@@ -0,0 +1,10 @@
+namespace SpravaObjednavek_GUI_App
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
